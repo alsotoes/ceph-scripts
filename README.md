a4fbdc54a45a99e10b43991c7ca7663104c2bd43
Alvaro Soto <alvaro@skyline.headup.local>
2016-03-10 22:45:40 -0600

    Solving problem in print csv file.

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 Let's see how it works ceph_space.py
 =======
 
-* TXT output
+* TXT output (any of the tree options print the same output)
 
 
+	$ python ceph_space.py "Ceph Cluster"
+	$ python ceph_space.py --output txt "Ceph Cluster"
 	$ python ceph_space.py --output txt --ratio 0.2 "Ceph Cluster"
 	
 	 	Cluster codename: Ceph Cluster
@@ -19,9 +21,22 @@ Let's see how it works ceph_space.py
 	 		 	Percentage Available: 75%
 
 
-* CSV output
+* CSV print 
 
 	$ python ceph_space.py --output csv --ratio 0.2 "Ceph Cluster"
 	
 	 	Cluster Name,Cluster ID,Cluster MON,Total,Reserved,Usable,Used,Available,Percentage Available
 	 	Ceph Cluster,85289369-1173-44b9-8a4c-ab5c1b50270f,stor01.ceph.local,10.7TB,2.1TB,8.5TB,2.1TB,6.5TB,75%
+
+
+* CSV printing and leaving a CSV file named cluster_stats (change with --file) in CWD
+
+	$ python ceph_space.py --output csv --ratio 0.2 --file cluster_stats "Ceph Cluster"
+	
+	 	Cluster Name,Cluster ID,Cluster MON,Total,Reserved,Usable,Used,Available,Percentage Available
+	 	Ceph Cluster,85289369-1173-44b9-8a4c-ab5c1b50270f,stor01.ceph.local,10.7TB,2.1TB,8.5TB,2.1TB,6.5TB,75%
+
+	$ cat cluster_stats.csv
+
+		Cluster Name,Cluster ID,Cluster MON,Total,Reserved,Usable,Used,Available,Percentage Available
+		Ceph Cluster,85289369-1173-44b9-8a4c-ab5c1b50270f,stor01.ceph.local,10.7TB,2.1TB,8.5TB,2.1TB,6.5TB,75%
--- ceph_space.py
+++ ceph_space.py
@@ -13,6 +13,8 @@ import rados
 import math
 import sys
 import csv
+import re
+import os
 
 
 # http://stackoverflow.com/questions/1094841/
@@ -36,11 +38,11 @@ def args_parse():
                 ===============
             '''))
 
-    parser.add_argument("--config", type=str, default="/etc/ceph/ceph.conf", help='Ceph config file (ceph.conf)')
-    parser.add_argument("--ratio", type=float, default=0.0, help='Reserved storage ratio, by default 0.2 as a 20%%')
-    parser.add_argument("--output", type=str, required=True, choices=["csv", "txt"], help='Output format to print in stdout')
-    parser.add_argument("--file", type=str, required=False, help='Output file')
-    parser.add_argument("name", type=str, help='Cluster fancy name')
+    parser.add_argument("--config", type=str, default="/etc/ceph/ceph.conf", help='Ceph config file (ceph.conf).')
+    parser.add_argument("--ratio", type=float, default=0.2, help='Reserved storage ratio, by default 0.2 as a 20%%.')
+    parser.add_argument("--output", type=str, required=False, choices=["csv", "txt"], help='Output format to print in stdout.')
+    parser.add_argument("--file", type=str, required=False, help='Output CSV file, this option override --output.')
+    parser.add_argument("name", type=str, help='Cluster fancy name.')
 
     return parser.parse_args()
 
@@ -62,19 +64,22 @@ def print_txt(ceph):
     print "==============="
 
 
-def print_csv(ceph):
-    f = open('output.csv', 'wt')
+def print_csv(ceph, filename, let):
+    f = open(filename+".csv", 'wt')
     try:
         writer = csv.writer(f)
-        writer.writerow("Cluster Name", "Cluster ID", "Cluster MON", "Total", "Reserved", "Usable", "Used", "Available", "Percentage Available")
-        writer.writerow(ceph['name'], ceph['fsid'], ceph['mon'], sizeof_fmt(ceph['total'] * 1024),
+        writer.writerow(["Cluster Name", "Cluster ID", "Cluster MON", "Total", "Reserved", "Usable", "Used", "Available", "Percentage Available"])
+        writer.writerow([ceph['name'], ceph['fsid'], ceph['mon'], sizeof_fmt(ceph['total'] * 1024),
             sizeof_fmt(ceph['reserved'] * 1024), sizeof_fmt(ceph['usable'] * 1024),
             sizeof_fmt(ceph['used'] * 1024), sizeof_fmt(ceph['available'] * 1024),
-            str(ceph['p_available']) + "%")
+            str(ceph['p_available']) + "%"])
     finally:
         f.close()
 
-    print open('output.csv', 'rt').read()
+    print open(filename+".csv", 'rt').read()
+    
+    if not let:
+        os.remove(filename+".csv")
 
 def get_cluster_info(cluster, ceph):
     ceph['default_size'] = int(cluster.conf_get('osd pool default size'))
@@ -105,7 +110,16 @@ def main():
     cluster_stats = get_cluster_info(cluster, ceph)
     get_and_calculate(cluster_stats, ceph, args)
 
-    print_txt(ceph) if "txt" == args.output else print_csv(ceph)
+    if not args.output:
+        args.output = "txt"
+
+    if not args.file:
+        args.file = "output"
+        let = 0
+    else:
+        let = 1
+
+    print_txt(ceph) if "txt" == args.output else print_csv(ceph, re.sub('[^0-9a-zA-Z]+', '_', args.file), let)
 
 
 # Start program
